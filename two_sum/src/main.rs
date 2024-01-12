b36a4af36207ad624ce49284e646f7b004cdf902
Chen Liang <chen.liang@ie.ibm.com>
2024-01-12 00:10:32 +0000

    HashMap solution for two sum

--- two_sum/src/main.rs
+++ two_sum/src/main.rs
@@ -1,9 +1,33 @@
+use std::collections::HashMap;
+
 fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    let mut h_map = HashMap::new();
+
+    for (i, y) in nums.iter().enumerate() {
+        let x = target - y;
+
+        if let Some(&j) = h_map.get(&x) {
+            return vec![j as i32, i as i32];
+        }
+
+        h_map.insert(y, i);
+    }
+
+    // let mut v_map: Vec<i32> = Vec::new();
+    //
+    // for (i, y) in nums.iter().enumerate() {
+    //     let x = target - y;
+    //
+    //     if let Some(j) = v_map.iter().position(|k| *k == x) {
+    //         return vec![j as i32, i as i32];
+    //     }
+    //     v_map.push(*y)
+    // }
 
     vec![0, 0]
 }
 
 fn main() {
-    let nums = vec![1, 2, 3];
-    println!("Result: {:?}", two_sum(nums, 3));
+    let nums = vec![3, 2, 4];
+    println!("Result: {:?}", two_sum(nums, 6));
 }
