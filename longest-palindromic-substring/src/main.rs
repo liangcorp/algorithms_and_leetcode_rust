773cf155a43fb2281bda23fe0318e72ce814e8c1
Chen Liang <chen.liang@ie.ibm.com>
2024-01-20 01:16:28 +0000

    Add timer

--- longest-palindromic-substring/src/main.rs
+++ longest-palindromic-substring/src/main.rs
@@ -1,13 +1,30 @@
 mod longest_palindromic;
 
-use longest_palindromic as lp;
+use std::time::SystemTime;
+
+use longest_palindromic::brute::find_string as brute;
+use longest_palindromic::center::find_string as center;
 
 fn main() {
-    println!("{}", lp::center::find_string(String::from("babad")));
-    println!("{}", lp::center::find_string(String::from("cbbd")));
-    println!("{}", lp::center::find_string(String::from("a")));
-    // println!("{}", lp::center::find_string(String::from("abb")));
-    // println!("{}", lp::center::find_string(String::from("bb")));
-    println!("{}", lp::center::find_string(String::from("aacabdkacaa")));
-    println!("{}", lp::brute::find_string(String::from("aacabdkacaa")));
+    // println!("{}", center(String::from("babad")));
+    // println!("{}", center(String::from("cbbd")));
+    // println!("{}", center(String::from("a")));
+    // println!("{}", center(String::from("abb")));
+    // println!("{}", center(String::from("bb")));
+
+    let now = SystemTime::now();
+    println!("{}", center(String::from("aacabdkacaa")));
+
+    match now.elapsed() {
+        Ok(elapsed) => println!("Center method in nano seconds: {}", elapsed.as_nanos()),
+        Err(e) => println!("Error: {e:?}"),
+    }
+
+    let now = SystemTime::now();
+    println!("{}", brute(String::from("aacabdkacaa")));
+
+    match now.elapsed() {
+        Ok(elapsed) => println!("Brute force method in nano seconds: {}", elapsed.as_nanos()),
+        Err(e) => println!("Error: {e:?}"),
+    }
 }
