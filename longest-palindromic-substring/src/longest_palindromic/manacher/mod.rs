1603400db95463ef404a164632e8ccf35f4b5bac
Chen Liang <chen.liang@ie.ibm.com>
2024-01-20 23:04:57 +0000

    code clean up

--- longest-palindromic-substring/src/longest_palindromic/manacher/mod.rs
+++ longest-palindromic-substring/src/longest_palindromic/manacher/mod.rs
@@ -14,17 +14,20 @@ pub fn find_string(s: String) -> String {
 
     let mut c = 1; // center of palnidormic
     let mut r = 0; // right limit of palnidormic
-    let mut l; // left limit of palanidromic
+    let mut i_mirror; // left limit of palanidromic
+
+    let mut max_radius = 0;
+    let mut max_index = 0;
 
     for i in 1..(new_s.len() - 1) {
         // find the corresponding letter in the palidrome subString
-        l = c - (i - c);
+        i_mirror = c - (i - c);
 
         if r > i {
-            p_radii[i] = if r - i < p_radii[l] {
+            p_radii[i] = if r - i < p_radii[i_mirror] {
                 r - i
             } else {
-                p_radii[i]
+                p_radii[i_mirror]
             }
         }
 
@@ -38,17 +41,13 @@ pub fn find_string(s: String) -> String {
             c = i; // next c = i
             r = i + p_radii[i];
         }
-    }
 
-    let mut max_index = 0;
-    let mut max_radius = 0;
-
-    for (i, &m) in p_radii.iter().enumerate() {
-        if max_radius < m {
-            max_radius = m;
+        if max_radius < p_radii[i] {
+            max_radius = p_radii[i];
             max_index = i;
         }
     }
+
     // Find the longest palindromic substring using index of character
     //      with the max palindromic radius
     // Filter out the '#'
