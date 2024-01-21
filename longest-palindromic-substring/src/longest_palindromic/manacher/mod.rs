53476c1a5b8a72456a1a760abb90db303fa166d3
Chen Liang <chen.liang@ie.ibm.com>
2024-01-21 01:50:18 +0000

    add better comments

--- longest-palindromic-substring/src/longest_palindromic/manacher/mod.rs
+++ longest-palindromic-substring/src/longest_palindromic/manacher/mod.rs
@@ -13,8 +13,8 @@ pub fn find_string(s: String) -> String {
     let mut p_radii = vec![0; new_s.len()];
 
     let mut c = 1; // center of palnidormic
-    let mut r = 0; // right limit of palnidormic
-    let mut i_mirror; // left limit of palanidromic
+    let mut r = 0; // right limit of palnidorm
+    let mut i_mirror; // left limit of palanidrom
 
     let mut max_radius = 0;
     let mut max_index = 0;
@@ -23,25 +23,36 @@ pub fn find_string(s: String) -> String {
         // find the corresponding letter in the palidrome subString
         i_mirror = c - (i - c);
 
+        // if current char is inside the border of
+        //  the largest palidrome
         if r > i {
             p_radii[i] = if r - i < p_radii[i_mirror] {
-                r - i
+                // if mirror character's palidrome
+                // expends beyond the border of the
+                // largest palidrome
+                r - i // palidrome of current character is at least
+                      // the size of (border - index)
             } else {
-                p_radii[i_mirror]
+                p_radii[i_mirror] // mirror property
             }
         }
 
-        // expanding around c i
+        // expanding around index
+        // starting point depending on (border - index) if applicable
         while new_s[i + 1 + p_radii[i]] == new_s[i - 1 - p_radii[i]] {
             p_radii[i] += 1;
         }
 
-        // Update c,r in case if the palindrome ced at i expands past r,
+        // Update center and border in case if the palindrome
+        // centered at i expands past border
+        // (i.e. a larger palidrome),
         if (i + p_radii[i]) > r {
             c = i; // next c = i
             r = i + p_radii[i];
         }
 
+        // Keep track of the index of char with the
+        // largest palidrome and it's radius
         if max_radius < p_radii[i] {
             max_radius = p_radii[i];
             max_index = i;
