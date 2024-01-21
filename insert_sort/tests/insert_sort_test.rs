df28c86df1e162459610951240312c09a7916bcc
Chen Liang <chen.liang@ie.ibm.com>
2024-01-21 19:02:37 +0000

    add module

--- insert_sort/tests/insert_sort_test.rs
+++ insert_sort/tests/insert_sort_test.rs
@@ -1,7 +1,10 @@
+use insert_sort::insert_sort::backward as bw;
+
 #[test]
 fn insert_sort_test() {
     let mut array = [30, 10, 60, 70, 50, 30, 20, 80, 40, 90];
-    computer_algorithms::insert_sort::backward::mono_increasing(&mut array);
     let want = [10, 20, 30, 30, 40, 50, 60, 70, 80, 90];
+
+    bw::mono_increasing(&mut array);
     assert_eq!(array, want);
 }
