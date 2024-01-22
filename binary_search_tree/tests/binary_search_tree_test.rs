70dc7232a33e3346631c2fd018e66c66ab5d7520
Chen Liang <chen.liang@ie.ibm.com>
2024-01-22 00:26:30 +0000

    create test case

---
+++ binary_search_tree/tests/binary_search_tree_test.rs
@@ -0,0 +1,14 @@
+use binary_search_tree::binary_search_tree::Node as bst_node;
+
+#[test]
+fn binary_search_tree_test() {
+    let mut tree_node = bst_node::new(50);
+    tree_node.insert(30);
+    tree_node.insert(20);
+    tree_node.insert(40);
+    tree_node.insert(70);
+    tree_node.insert(60);
+    tree_node.insert(80);
+
+    assert_eq!(7, tree_node.node_count());
+}
