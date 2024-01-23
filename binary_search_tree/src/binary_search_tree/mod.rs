c6855c070e737760667bb355944c3efd66cdc635
Chen Liang <chen.liang@ie.ibm.com>
2024-01-23 16:28:57 +0000

    clean up

--- binary_search_tree/src/binary_search_tree/mod.rs
+++ binary_search_tree/src/binary_search_tree/mod.rs
@@ -1,6 +1,4 @@
-mod delete;
 mod print;
-mod search;
 
 use std::cmp::Ordering;
 
@@ -86,6 +84,35 @@ impl Node {
         match self.left.as_ref() {
             Some(left) => left.min_value(),
             None => self.data,
+        }
+    }
+
+    pub fn delete(&mut self, data: i32) {
+        match data.cmp(&self.data) {
+            Ordering::Less => {
+                if let Some(left) = self.left.as_mut() {
+                    left.delete(data);
+                }
+            }
+            Ordering::Greater => {
+                if let Some(right) = self.right.as_mut() {
+                    right.delete(data);
+                }
+            }
+
+            Ordering::Equal => {
+                // if self.left.is_none() && self.right.is_none() {
+                //     // let _ = self;
+                //     println!("{:?}", self);
+                //     drop(self);
+                //     self.data = 0;
+                //
+                // } else if self.left.is_some() && self.right.is_none() {
+                //     self::Node::new(self.left.as_ref().unwrap().data);
+                // } else if self.left.is_none() && self.right.is_some() {
+                //     self::Node::new(self.right.as_ref().unwrap().data);
+                // }
+            }
         }
     }
 }
