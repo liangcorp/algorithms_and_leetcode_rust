dd5f78ae076710d2ba83b399c2a90869f3b6846d
Chen Liang <chen.liang@ie.ibm.com>
2024-01-25 02:25:24 +0000

    completed the delet function

--- binary_search_tree/src/lib.rs
+++ binary_search_tree/src/lib.rs
@@ -1,6 +1,5 @@
 mod print;
 
-use std::borrow::BorrowMut;
 use std::cell::RefCell;
 use std::cmp::Ordering;
 use std::rc::Rc;
@@ -97,7 +96,7 @@ impl Node {
     // DONE Node to be deleted has one child:
     //  You can just replace the node with the child node.
     //
-    // @TODO: Node to be deleted has two child:
+    // DONE: Node to be deleted has two child:
     //  Need to figure out what will be the
     //  replacement of the node to be deleted.
     //  Want minimal disruption to the existing tree structure
@@ -108,82 +107,74 @@ impl Node {
     //  If taking if from the right subtree,
     //      we have to take the smallest value in the right subtree.
     //  Choose one approach and stick to it.
+    //
+    //  Currently using Max element at left method
     pub fn delete(&mut self, data: i32) {
-        // Only delete leaf node at the moment.
-        // Taking a break from borrow checker hell
         match data.cmp(&self.data) {
             Ordering::Less => {
                 if let Some(left) = &self.left {
-                    println!("Debug left");
                     if (**left).borrow_mut().data == data {
-                        println!("Found left");
                         if (**left).borrow_mut().left.is_none()
                             && (**left).borrow_mut().right.is_none()
                         {
-                            println!("Delete left leaf node");
+                            // delete leaf node at right
                             self.left = None;
                         } else if (**left).borrow_mut().left.is_some()
                             && (**left).borrow_mut().right.is_none()
                         {
                             let temp = (**left).borrow_mut().left.clone();
-                            println!("Debug left left: {:?}", temp);
+                            // replace node with left child;
                             self.left = temp;
                         } else if (**left).borrow_mut().left.is_none()
                             && (**left).borrow_mut().right.is_some()
                         {
                             let temp = (**left).borrow_mut().right.clone();
-                            println!("Debug left right: {:?}", temp);
+                            // replace node with right child;
                             self.left = temp;
                         } else {
-                            println!("delete left");
                             (**left).borrow_mut().delete(data);
                         }
                     } else {
-                        println!("delete leftward");
                         (**left).borrow_mut().delete(data);
                     }
                 }
             }
             Ordering::Greater => {
                 if let Some(right) = &self.right {
-                    println!("Debug right");
                     if (**right).borrow_mut().data == data {
-                        println!("Found right");
                         if (**right).borrow_mut().left.is_none()
                             && (**right).borrow_mut().right.is_none()
                         {
-                            println!("Delete right leaf node");
+                            // delete leaf node
                             self.right = None;
                         } else if (**right).borrow_mut().left.is_some()
                             && (**right).borrow_mut().right.is_none()
                         {
                             let temp = (**right).borrow_mut().left.clone();
-                            println!("Debug right left: {:?}", temp);
+                            // replace node with left child
                             self.right = temp;
                         } else if (**right).borrow_mut().left.is_none()
                             && (**right).borrow_mut().right.is_some()
                         {
                             let temp = (**right).borrow_mut().right.clone();
-                            println!("Debug right right: {:?}", temp);
+                            // replace node with right child
                             self.right = temp;
                         } else {
-                            println!("delete right");
                             (**right).borrow_mut().delete(data);
                         }
                     } else {
-                        println!("delete leftward");
                         (**right).borrow_mut().delete(data);
                     }
                 }
             }
             Ordering::Equal => {
-                println!("Delete equal node");
+                // find large node at the left
                 let max_left_value = self.max_left();
+                //  recursively call delete function
+                //  to delete max node at left
+                self.delete(max_left_value);
+                // replace value of found node
                 self.data = max_left_value;
-                if let Some(left) = &self.left {
-                    println!("delete max left");
-                    (**left).borrow_mut().delete(max_left_value);
-                }
             }
         }
     }
