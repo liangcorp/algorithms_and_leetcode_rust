8c6a54235ab4ae87f710f4cd9f64cbac3ab7a883
Chen Liang <chen.liang@ie.ibm.com>
2024-01-28 00:44:53 +0000

    accepted answer

--- zigzag-conversion/src/zigzag/mod.rs
+++ zigzag-conversion/src/zigzag/mod.rs
@@ -9,7 +9,7 @@ pub fn convert(s: String, num_rows: i32) -> String {
     let mut slice_start = 0;
     let mut slice_end = num_rows + num_rows - 2;
     let distance = num_rows + num_rows - 2;
-    let mut string_chucks: Vec<&str> = vec![];
+    let mut str_chucks: Vec<&str> = vec![];
 
     if num_rows == 1 {
         return s;
@@ -17,33 +17,43 @@ pub fn convert(s: String, num_rows: i32) -> String {
 
     // divide the string into chucks of size = num_rows + num_rows - 2
     while (slice_end as usize) < s.len() {
-        string_chucks.push(&s[slice_start as usize..slice_end as usize]);
+        str_chucks.push(&s[slice_start as usize..slice_end as usize]);
         slice_start = slice_end;
         slice_end += distance;
     }
-    string_chucks.push(&s[slice_start as usize..]);
+    let filer_str = "#".repeat(distance as usize - s[slice_start as usize..].len());
+    let temp = format!("{}{}", &s[slice_start as usize..], &filer_str);
+    str_chucks.push(&temp);
 
     for i in 0..num_rows {
-        for s_chuck in string_chucks.iter() {
+        for s_chuck in str_chucks.iter() {
             if i == 0 {
-                // add top characters of the string chucks to the vector
+                // add top characters of the string chucks to
+                // the vector
                 new_s.push(s_chuck.chars().next().unwrap());
-            } else if i == num_rows - 1 || i as usize == s_chuck.len() - 1 {
+            } else if i == num_rows - 1 {
                 // if reach center of string chuck
-                // or the length of string chuck is less than given row number
+                // or the length of string chuck is less than
+                // given row number
                 if let Some(c) = s_chuck.chars().nth(i as usize) {
-                    new_s.push(c);
+                    if c != '#' {
+                        new_s.push(c);
+                    }
                 }
-            } else if i as usize > s_chuck.len() - 1 {
-                // do nothing if the row number is larger than the string lentgh
             } else if i != num_rows - 1 {
-                // add the characters at the beginning and the end to the vector
+                // add the characters at the beginning and the end
+                // to the vector
+                // Filter out the added '#'
                 if let Some(c) = s_chuck.chars().nth(i as usize) {
-                    new_s.push(c);
+                    if c != '#' {
+                        new_s.push(c);
+                    }
                 }
 
                 if let Some(c) = s_chuck.chars().nth(s_chuck.len() - i as usize) {
-                    new_s.push(c);
+                    if c != '#' {
+                        new_s.push(c);
+                    }
                 }
             }
         }
