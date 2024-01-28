249e55ada3c64901b49b7851b6803dd52f8bde64
Chen Liang <chen.liang@ie.ibm.com>
2024-01-28 16:40:29 +0000

    Reorganise

--- zigzag-conversion/src/zigzag/calculate/mod.rs
+++ zigzag-conversion/src/zigzag/calculate/mod.rs
@@ -7,35 +7,23 @@
 /// of character that fits the following logic:
 ///  - row 0 are push into the new string
 ///      - if string index % distance == index
-///  - Find the index of head and tail of string that are row > 0
-///     i.e. (string index + num_rows index) % distance == 0
-///     Push the head and tail into the new string
+///  - rest of the strings's head and tail are pushed into the new string
+///     - (string index + num_rows index) % distance == 0
 pub fn convert(s: String, num_rows: i32) -> String {
     if num_rows == 1 {
         return s;
     }
 
     let mut new_s = String::new();
-    let distance = (num_rows * 2 - 2) as usize;
-    let mut head_index: usize;
+    let distance = num_rows + num_rows - 2;
 
-    for i in 0..num_rows as usize {
-        head_index = i;
-        while head_index < s.len() {
-            if (head_index + i) % distance == 0 {
-                if let Some(c) = s.chars().nth(head_index) {
-                    new_s.push(c);
-                }
-            } else if head_index % distance == i {
-                let tail_index = head_index + distance - 2 * i;
-                if let Some(c) = s.chars().nth(head_index) {
-                    new_s.push(c);
-                }
-                if let Some(c) = s.chars().nth(tail_index) {
+    for i in 0..num_rows {
+        for j in i as usize..s.len() {
+            if (j as i32) % distance == i || (j as i32 + i) % distance == 0 {
+                if let Some(c) = s.chars().nth(j) {
                     new_s.push(c);
                 }
             }
-            head_index += distance;
         }
     }
     new_s
--- zigzag-conversion/src/zigzag/linear/mod.rs
+++ zigzag-conversion/src/zigzag/linear/mod.rs
@@ -1,28 +1,42 @@
+/// image the string are broken into different chucks of
+/// size num_rows + num_rows - 2
+///
+/// create a loop that loops `num_rows` time
+///
+/// each loop go through the string and caluate the indexes
+/// of character that fits the following logic:
+///  - row 0 are push into the new string
+///      - if string index % distance == index
+///  - Find the index of head and tail of string that are row > 0
+///     i.e. (string index + num_rows index) % distance == 0
+///     Push the head and tail into the new string
 pub fn convert(s: String, num_rows: i32) -> String {
     if num_rows == 1 {
         return s;
     }
 
-    let num_rows = num_rows as usize;
-    let mut res = String::new();
-    let s_chars: Vec<char> = s.chars().collect();
+    let mut new_s = String::new();
+    let distance = (num_rows * 2 - 2) as usize;
+    let mut head_index: usize;
 
-    for r in 0..num_rows {
-        let increment = 2 * (num_rows - 1);
-        let mut i = r;
-
-        while i < s_chars.len() {
-            res.push(s_chars[i]);
-
-            if r > 0 && r < num_rows - 1 {
-                let next_index = i + increment - 2 * r;
-
-                if next_index < s_chars.len() {
-                    res.push(s_chars[next_index]);
+    for i in 0..num_rows as usize {
+        head_index = i;
+        while head_index < s.len() {
+            if (head_index + i) % distance == 0 {
+                if let Some(c) = s.chars().nth(head_index) {
+                    new_s.push(c);
+                }
+            } else if head_index % distance == i {
+                let tail_index = head_index + distance - 2 * i;
+                if let Some(c) = s.chars().nth(head_index) {
+                    new_s.push(c);
+                }
+                if let Some(c) = s.chars().nth(tail_index) {
+                    new_s.push(c);
                 }
             }
-            i += increment;
+            head_index += distance;
         }
     }
-    res
+    new_s
 }
