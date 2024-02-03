0651492a67c12f6208229747160aa6d50c530943
Chen Liang <chen.liang@ie.ibm.com>
2024-02-03 17:19:10 +0000

    hanlding '.' and no '.'

--- regular-expression-matching/src/lib.rs
+++ regular-expression-matching/src/lib.rs
@@ -1,22 +1,20 @@
 pub fn is_match(s: String, p: String) -> bool {
-    // let mut p_vec: Vec<char> = vec![];
+    if !p.contains('.') && !p.contains('*') && s.len() != p.len() {
+        false
+    } else if p.contains('.') && !p.contains('*') {
+        println!("{} {}", s, p);
+        if s.is_empty() && p.is_empty() {
+            return true;
+        }
 
-    // let mut s_i = 0;
-    // let mut p_i = 0;
+        if s.chars().next() == p.chars().next() || p.starts_with('.') {
+            is_match(s.as_str()[1..].to_string(), p.as_str()[1..].to_string());
+        }
 
-    if s.chars().next() == p.chars().next() || p.starts_with('*') {
-        is_match(s.as_str()[1..].to_string(), p.as_str()[1..].to_string());
-    } else if p.starts_with('*') {
-        is_match(
-            s.as_str()[2..].to_string(),
-            p.as_str()[2..].to_string(),
-        );
+        true
     } else {
-        return false;
+        false
     }
-
-    // s == String::from_iter(p_vec)
-    true
 }
 
 #[cfg(test)]
@@ -30,17 +28,26 @@ mod tests {
     }
 
     #[test]
-    fn test_is_match_2() {
-        assert!(is_match(String::from("aa"), String::from("a*")));
+    fn test_is_match_dot_only() {
+        assert!(is_match(String::from("ab"), String::from("a.")));
     }
 
     #[test]
-    fn test_is_match_3() {
-        assert!(is_match(String::from("aa"), String::from(".*")));
+    fn test_is_match_dot_only_2() {
+        assert!(is_match(String::from("abc"), String::from("a.c")));
     }
-
-    #[test]
-    fn test_is_match_4() {
-        assert!(is_match(String::from("aab"), String::from("c*a*b")));
-    }
+    // #[test]
+    // fn test_is_match_star_only() {
+    //     assert!(is_match(String::from("aa"), String::from("a*")));
+    // }
+    //
+    // #[test]
+    // fn test_is_match_3() {
+    //     assert!(is_match(String::from("aa"), String::from(".*")));
+    // }
+    //
+    // #[test]
+    // fn test_is_match_4() {
+    //     assert!(is_match(String::from("aab"), String::from("c*a*b")));
+    // }
 }
--- regular-expression-matching/src/main.rs
+++ regular-expression-matching/src/main.rs
@@ -1,5 +1,5 @@
 use regular_expression_matching::is_match;
 
 fn main() {
-    is_match(String::from("aa"), String::from(".*"));
+    println!("{}", is_match(String::from("aa"), String::from(".*")));
 }
