601ad4591b49a8cd809c13f972a8cbf37e2366c5
Chen Liang <chen.liang@ie.ibm.com>
2024-02-03 22:26:41 +0000

    code change

--- regular-expression-matching/src/brute/mod.rs
+++ regular-expression-matching/src/brute/mod.rs
@@ -7,7 +7,7 @@ pub fn is_match(s: String, p: String) -> bool {
         !s.is_empty() && (s.chars().next() == p.chars().next() || p.starts_with('.'));
 
     if p.len() >= 2 && p.chars().nth(1).unwrap() == '*' {
-        return is_match(s.clone(), p.as_str()[2..].to_string())
+        return is_match(s.as_str()[..].to_string(), p.as_str()[2..].to_string())
             || (is_first_match && is_match(s.as_str()[1..].to_string(), p));
     } else {
         return is_first_match
