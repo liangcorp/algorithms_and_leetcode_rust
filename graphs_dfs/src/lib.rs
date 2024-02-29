dca52ef74ce4d914b61286a60cc41d4a7e8dda12
Chen Liang <chen.liang@ie.ibm.com>
2024-02-29 15:10:05 +0000

    clean up

--- graphs_dfs/src/lib.rs
+++ graphs_dfs/src/lib.rs
@@ -77,15 +77,21 @@ mod tests {
     fn test_dfs() {
         let vertices = vec![1, 2, 3, 4, 5, 6, 7];
 
-        let edges = vec![(1, 2), (1, 3), (2, 4), (2, 5), (3,6), (3,7)];
+        let edges = vec![(1, 2), (1, 3), (2, 4), (2, 5), (3, 6), (3, 7)];
 
         let root = 1;
         let objective = 7;
 
-        let correct = vec![1,2 ,4, 5, 3, 6,7];
+        let correct = vec![1, 2, 4, 5, 3, 6, 7];
 
-        let graph = Graph::new(vertices.into_iter().map(|v| v.into()).collect(), edges.into_iter().map(|e| e.into()).collect());
+        let graph = Graph::new(
+            vertices.into_iter().map(|v| v.into()).collect(),
+            edges.into_iter().map(|e| e.into()).collect(),
+        );
 
-        assert_eq!(depth_first_search(&graph, root.into(), objective.into()), Some(correct));
+        assert_eq!(
+            depth_first_search(&graph, root.into(), objective.into()),
+            Some(correct)
+        );
     }
 }
